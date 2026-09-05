2648d287dbbdf3fd1eb08cabcb89ebf1899e97c3
Issa Alzarzour <darklord8756@gmail.com>
2026-09-05 10:42:20 +0200

    Fix health test expectation

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,5 +9,5 @@ client = TestClient(app)
 def test_health():
     response = client.get("/health")
 
-    assert response.status_code == 201
+    assert response.status_code == 200
     assert response.json() == {"status": "ok"}
